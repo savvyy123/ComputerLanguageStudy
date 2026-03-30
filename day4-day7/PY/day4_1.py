dff6c859f4722e1c95abc6f581ef2ad1a2483d04
savvyy123 <jwpm.llo1212@gmail.com>
2026-03-30 17:19:39 +0900

    day4

---
+++ day4-day7/PY/day4_1.py
@@ -0,0 +1,15 @@
+# score = 55 として、if / elif / else で評価を表示してください。
+# 80点以上 → "優秀"
+# 60点以上 → "合格"
+# 60点未満 → "不合格"
+# 期待される出力：不合格
+
+score = 55
+
+# ここに書いてね
+if score >= 80:
+    print("great")
+elif score >= 60:
+    print("pass")
+else:
+    print("fail")
---
+++ day4-day7/PY/day4_3.py
@@ -0,0 +1,11 @@
+# 好きな色を color に入れて、
+# ["red", "blue", "green"] に含まれるなら "基本色です"
+# 含まれないなら "その他の色です" と表示してください。
+# （color = "red" なら "基本色です" が出力されます）
+
+color = "red"
+
+# ここに書いてね
+if color in ["red", "blue", "green"]:
+    print("基本色です")
+else:    print("その他の色です")
